383aa8301efd2f1a62d464d01f343a90e018dbed
Manerton <assbanlucky@gmail.com>
2024-01-05 22:23:06 +0400

    edit types

--- types/note.py
+++
@@ -1,8 +0,0 @@
-class Note:
-    id_note: str
-    id_category: str 
-    description: str
-    
-    def __init__(self, id_category: str, description: str) -> None:
-        self.id_category = id_category
-        self.description = description
